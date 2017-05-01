f574c7d42bf06279c06d4dc4084574c52341a408
benedictb <becker.benedictj@gmail.com>
2017-04-30 21:53:57 -0400

    just organized a couple of things re: which program runs where

--- home.py
+++ home.py
@@ -8,6 +8,8 @@ COMMAND_PORT = 40678
 CLIENT_PORT = 41678
 DATA_PORT = 42678
 
+# home runs on ash
+
 connections = dict()
 reactor.listenTCP(CLIENT_PORT, ClientConnectionFactory(connections))
 reactor.listenTCP(COMMAND_PORT, CommandHomeConnectionFactory(connections))
--- work.py
+++ work.py
@@ -2,10 +2,13 @@
 
 from twisted.internet import reactor
 from command_connection import CommandWorkConnectionFactory
+import sys
 
 COMMAND_PORT = 40678
 CLIENT_PORT = 41678
 DATA_PORT = 42678
+
+# work runs on newt
 
 connections = dict()
 reactor.connectTCP('ash.campus.nd.edu', COMMAND_PORT, CommandWorkConnectionFactory(connections))
